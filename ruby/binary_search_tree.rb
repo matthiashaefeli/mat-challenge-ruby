bb62dae924cee1211b759c8ed5b37e9e29f0f9d9
matthiashaefeli <haefeli.matthias@gmail.com>
2018-08-05 16:23:53 -0500

    finished binary missing one test

--- ruby/binary_search_tree.rb
+++ ruby/binary_search_tree.rb
@@ -1,8 +1,10 @@
 class Bst
 
-  def initialize(number)
+  def initialize(number, tree = { number => [nil, nil] })
     @first = number
-    @tree = {}
+    @tree = tree
+    @array = []
+    @array.push(number)
   end
 
   def data
@@ -13,24 +15,40 @@ class Bst
     @tree
   end
 
-  def insert(new_number)
-    if new_number <= @first
-      @tree[@first] = [new_number, nil]
+  def insert(new_number, node = @first)
+    if new_number <= node
+      if @tree[node][0] == nil 
+        @tree[node][0] = new_number
+        @tree[new_number] = [nil, nil]
+        @array.push(new_number)
+      else 
+        insert(new_number, @tree[node][0])
+      end
     else 
-      @tree[@first] = [nil, new_number]
+      if @tree[node][1] == nil
+        @tree[node][1] = new_number
+        @tree[new_number] = [nil, nil]
+        @array.push(new_number)
+      else
+        insert(new_number, @tree[node][1])
+      end
     end
   end
 
   def left
-    Bst.new(search(@first, 0))
+    Bst.new(search(@first, 0), @tree)
   end
 
   def right
-    Bst.new(search(@first, 1))
+    Bst.new(search(@first, 1), @tree)
   end
 
   def search(f, n)
     @tree[f][n]
+  end
+
+  def each(&block)
+    @array.sort.each(&block)
   end
 
 
@@ -39,4 +57,5 @@ end
 # four = Bst.new 4
 # four.insert 2
 # four.data
+# four.tree
 # four.left.data
